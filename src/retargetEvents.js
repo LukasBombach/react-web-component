921b93b0becd17e3b04efbd22190cab90ae35b4a
Lukas Bombach <lukas.bombach@googlemail.com>
2017-08-14 12:44:12 +0200

    fix nested elements events

--- src/retargetEvents.js
+++ src/retargetEvents.js
@@ -1,34 +1,39 @@
 module.exports = function retargetEvents(shadowRoot) {
-  const events = 'onClick onContextMenu onDoubleClick onDrag onDragEnd onDragEnter onDragExit onDragLeave onDragOver onDragStart onDrop onMouseDown onMouseEnter onMouseLeave onMouseMove onMouseOut onMouseOver onMouseUp'.split(
-    ' '
-  );
 
-  function dispatchEvent(event, eventType, itemProps) {
-    if (itemProps[eventType]) {
-      itemProps[eventType](event);
-    } else if (itemProps.children) {
-      for (var i in itemProps.children) {
-        if (itemProps.children.hasOwnProperty(i) && itemProps.children[i].props) dispatchEvent(event, eventType, itemProps.children[i].props);
-      }
-    }
-  }
+  const events = 'onClick onContextMenu onDoubleClick onDrag onDragEnd onDragEnter onDragExit onDragLeave onDragOver onDragStart onDrop onMouseDown onMouseEnter onMouseLeave onMouseMove onMouseOut onMouseOver onMouseUp'.split(' ')
 
-  // Compatible with v0.14 & 15
-  function findReactInternal(item) {
-    for (var i in item) {
-      if (item.hasOwnProperty(i) && i.indexOf('_reactInternal') !== -1) {
-        return item[i];
-      }
-    }
-  }
-  for (var i in events) {
-    const eventType = events[i];
+  for (const eventType of events) {
     const transformedEventType = eventType.replace(/^on/, '').toLowerCase();
-    shadowRoot.addEventListener(transformedEventType, function(event) {
-      const internalComponent = findReactInternal(event.target);
-      if (internalComponent && internalComponent._currentElement && internalComponent._currentElement.props) {
-        dispatchEvent(event, eventType, internalComponent._currentElement.props);
+
+    shadowRoot.addEventListener(transformedEventType, function (event) {
+      for (const item of event.path) {
+        const internalComponent = findReactInternal(item);
+
+        if (internalComponent && internalComponent._currentElement && internalComponent._currentElement.props) {
+          dispatchEvent(event, eventType, internalComponent._currentElement.props);
+        }
+
+        if (item === shadowRoot) {
+          break;
+        }
       }
     });
   }
 };
+
+function findReactInternal(item) {
+  let instance;
+  for (let key in item) {
+    if (item.hasOwnProperty(key) && key.indexOf('_reactInternal') !== -1) {
+      instance = item[key];
+      break;
+    }
+  }
+  return instance;
+}
+
+function dispatchEvent(event, eventType, itemProps) {
+  if (itemProps[eventType]) {
+    itemProps[eventType](event);
+  }
+}
